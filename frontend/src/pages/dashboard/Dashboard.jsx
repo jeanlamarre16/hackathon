fe38eaa1a9190fdd849f6c8440bab813c10a18ac
Attila Simon <simon.attila.fr@gmail.com>
2022-06-30 23:41:44 +0200

    Animation added to Login

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../../components/navbar/Navbar";
 import DashboardCategories from "@components/dashboard_categories/DashboardCategories";
+import Navbar from "../../components/navbar/Navbar";
 
 import "./Dashboard.css";
 
