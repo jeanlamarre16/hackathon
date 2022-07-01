00e3a25d80937f753723c76a73cd40ff09be1b46
JeromeVigneron <jerome.vigneron@net-c.fr>
2022-07-01 10:46:29 +0200

    dev - fix: change the visual for the navbar & add it to the project page

--- frontend/src/pages/project/Project.jsx
+++ frontend/src/pages/project/Project.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Navbar from "@components/navbar/Navbar";
+
 import "./Project.css";
 
 export default function Project() {
@@ -13,46 +15,51 @@ export default function Project() {
   }, []);
 
   return (
-    <div className="div-project">
-      <h1>Nos Projets</h1>
-      <div className="container-bloc-affichage">
-        <div className="blocAffichage">
-          {projects.map((project) => {
-            return (
-              <>
-                <h2>{project.project_domain}</h2>
-                <p>
-                  {`${project.project_name} | ${project.created_at} | ${project.id_city}`}{" "}
-                  |<i className="fa-solid fa-pencil" /> |
-                  <Link to={`./${project.id}`}>
-                    <i className="fa-solid fa-user-plus" />
-                  </Link>
-                </p>
-                <progress value={project.progression} max="100">
-                  {project.progression}
-                </progress>
-              </>
-            );
-          })}
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <div className="div-project">
+        <h1>Nos Projets</h1>
+        <div className="container-bloc-affichage">
+          <div className="blocAffichage">
+            {projects.map((project) => {
+              return (
+                <>
+                  <h2>{project.project_domain}</h2>
+                  <p>
+                    {`${project.project_name} | ${project.created_at} | ${project.id_city}`}{" "}
+                    |<i className="fa-solid fa-pencil" /> |
+                    <Link to={`./${project.id}`}>
+                      <i className="fa-solid fa-user-plus" />
+                    </Link>
+                  </p>
+                  <progress value={project.progression} max="100">
+                    {project.progression}
+                  </progress>
+                </>
+              );
+            })}
+          </div>
+          <div className="blocAffichage">
+            {projects.map((project) => {
+              return (
+                <>
+                  <h2>&nbsp;</h2>
+                  <p>{`${project.project_name} | ${project.created_at} | ${project.id_city}`}</p>
+                  <progress value={project.progression} max="100">
+                    {project.progression}
+                  </progress>
+                </>
+              );
+            })}
+          </div>
         </div>
-        <div className="blocAffichage">
-          {projects.map((project) => {
-            return (
-              <>
-                <h2>&nbsp;</h2>
-                <p>{`${project.project_name} | ${project.created_at} | ${project.id_city}`}</p>
-                <progress value={project.progression} max="100">
-                  {project.progression}
-                </progress>
-              </>
-            );
-          })}
+        <div className="div-button">
+          <button type="button">Ajouter un projet</button>
+          <button type="button">Logout</button>
         </div>
       </div>
-      <div className="div-button">
-        <button type="button">Ajouter un projet</button>
-        <button type="button">Logout</button>
-      </div>
-    </div>
+    </>
   );
 }
